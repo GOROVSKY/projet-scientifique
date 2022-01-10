815535a7dbc36bdcbe9e9545f242621b9f52d832
Driss <drissdutto@hotmail.fr>
2022-01-10 17:11:27 +0100

    Corrected duplicate value bug

--- Iot/data_collector_and_send_to_uart.py
+++ Iot/data_collector_and_send_to_uart.py
@@ -11,12 +11,12 @@ SERIALPORT = "/dev/ttyS5"
 BAUDRATE = 115200
 ser = serial.Serial()
 IP_HOST = "127.0.0.1"
-value_dict= dict()
-
+key = b'\xa1\x52\xff\x94\xc6\xf5\x0d\xbc\x48\x98\xb6\x9d\xc7\xaf\x6d\x1b\x44\x35\xf8\x81\xe7\x65\x8b\x9b\xd5\xd4\x16\xd5\x4a\x97\x0f\x8d'
 
 def get_data_from_db_and_send_to_uart():
     global ser
     nb_try=0
+    msg_list = list()
     while nb_try<3:
         try:
             req = requests.get('http://%s:5000/api/sensors' % (IP_HOST))
@@ -27,32 +27,33 @@ def get_data_from_db_and_send_to_uart():
                     sensor_id = int(row['id'])
                     msg_type = int(row['type_id'])
                     value = int(row['value'])
-                    if sensor_id in value_dict:
-                        value_dict[sensor_id][msg_type]=value
-                    else:
-                        value_dict[sensor_id]=dict()
-                        value_dict[sensor_id][msg_type]=value
+                    msg_as_list=list()
+                    msg_as_list.append(sensor_id)
+                    msg_as_list.append(msg_type)
+                    msg_as_list.append(value)
+                    msg_list.append(msg_as_list)
                     print("id=%d,type=%d,value=%d" % (sensor_id,msg_type,value))
                 
                 #print("VALUE DICT")
                 msg = b''
                 nb_element_to_send=0
-                for sensor_id in value_dict:
-                    for msg_type in value_dict[sensor_id]:
-                        value= value_dict[sensor_id][msg_type]
-                        #print("id=%d,type=%d,value=%d" % (sensor_id,msg_type,value_dict[sensor_id][msg_type]))
-                        sensor_id_as_bytes = sensor_id.to_bytes(2,byteorder='big')
-                        msg_type_as_bytes =  msg_type.to_bytes(1,byteorder='big') 
-                        value_as_bytes = value.to_bytes(1,byteorder='big')
-                        msg+= sensor_id_as_bytes+msg_type_as_bytes+value_as_bytes
-                        nb_element_to_send+=1
-                        if(nb_element_to_send==60):
-                            nb_element_to_send_as_bytes =  nb_element_to_send.to_bytes(1,byteorder='big')
-                            ser.write(nb_element_to_send_as_bytes+msg)
-                            print("Message <" + str(nb_element_to_send_as_bytes+msg) + "> <"+str(len(msg))+"> sent to micro-controller." )
-                            msg=b''
-                            nb_element_to_send=0
-                            time.sleep(0.05)
+                for msg_as_list in msg_list:
+                    value= msg_as_list[2]
+                    sensor_id=msg_as_list[0]
+                    msg_type=msg_as_list[1]
+                    #print("id=%d,type=%d,value=%d" % (sensor_id,msg_type,value_dict[sensor_id][msg_type]))
+                    sensor_id_as_bytes = sensor_id.to_bytes(2,byteorder='big')
+                    msg_type_as_bytes =  msg_type.to_bytes(1,byteorder='big') 
+                    value_as_bytes = value.to_bytes(1,byteorder='big')
+                    msg+= sensor_id_as_bytes+msg_type_as_bytes+value_as_bytes
+                    nb_element_to_send+=1
+                    if(nb_element_to_send==60):
+                        nb_element_to_send_as_bytes =  nb_element_to_send.to_bytes(1,byteorder='big')
+                        ser.write(nb_element_to_send_as_bytes+msg)
+                        print("Message <" + str(nb_element_to_send_as_bytes+msg) + "> <"+str(len(msg))+"> sent to micro-controller." )
+                        msg=b''
+                        nb_element_to_send=0
+                        time.sleep(0.05)
 
                 if(nb_element_to_send>0):
                     nb_element_to_send_as_bytes =  nb_element_to_send.to_bytes(1,byteorder='big')
